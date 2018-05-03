73050cee9480eab4f376ba56242a3cbd1bcb7e7a
Shurjo Maitra <shurjom@gmail.com>
2018-05-03 14:26:38 -0400

    led works with this

---
+++ app/src/main/java/edu/calvin/sm47/hotjac/SplashScreen.java
@@ -0,0 +1,46 @@
+package edu.calvin.sm47.hotjac;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by PEACE on 3/30/2016.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        //Makes the title bar disappear
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_status);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(700);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,DeviceList.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
--- app/src/main/java/edu/calvin/sm47/hotjac/StatusFragment.java
+++ app/src/main/java/edu/calvin/sm47/hotjac/StatusFragment.java
@@ -9,8 +9,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
+
+
+import java.io.IOException;
+import java.util.UUID;
+
 
 
 /**
